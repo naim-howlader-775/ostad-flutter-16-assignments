5e42c16c40bc0ff38f9a28218307405b36cfa31a
Naim Howlader <naimmd775@gmail.com>
2026-06-20 14:10:56 +0600

    complete 1

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:of_assignments/repository_guide_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:of_assignments/screens/student_profile_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Ostad Flutter 16 Assignments",
-      debugShowCheckedModeBanner: false,
-      initialRoute: 'base',
-      home: RepositoryGuideScreen(),
+    return ScreenUtilInit(
+      designSize: Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_,child){
+        return MaterialApp(
+          title: "Ostad Flutter 16 Assignments",
+          debugShowCheckedModeBanner: false,
+          home: StudentProfileScreen(),
+        );
+      },
     );
   }
 }
